d7092fcdab03ec9c39f91491e0512ba553e4a8fa
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-02-22 11:31:39 -0500

    added functionality to repolink.js and index.js

--- controllers/repolink.js
+++ controllers/repolink.js
@@ -1,1 +1,12 @@
-//saves in github
+const express = require('express')
+const router = express.Router()
+
+const RepoLink = require('../models/RepoLink')
+
+router.get('/', (req, res) => {
+  RepoLink.find({}).then(repolinks => {
+      res.render('post/index', {repolinks})
+    })
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const bodyParser = require('body-parser')
 const hbs = require('hbs')
 
 app.set('view engine', 'hbs')
+const repoController = require('./controllers/repolink')
 
 app.get('/', (req, res) => {
     res.render('index')
 })
+
+app.use('/repolinks', repoController)
 
 app.listen(4000, () => {
     console.log('server running')
