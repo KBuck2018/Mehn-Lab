96f98fb4ad62e9209cb19e422fb492182e17b1d8
Kevin Buckley <kevin.buckley2013@gmail.com>
2018-02-23 15:18:24 -0500

    added user.js controller features

--- controllers/user.js
+++ controllers/user.js
@@ -4,4 +4,43 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const passport = require('passport')
 
+// GET /signup
+router.get('/signup', (req, res) => {
+    res.render('signup', {message: req.flash("signupMessage")})
+  })
+  
+  // POST /signup
+  router.post('/signup', (req, res) => {
+    var signupStrategy = passport.authenticate('local-signup', {
+      successRedirect : '/',
+      failureRedirect : '/signup',
+      failureFlash : true
+    });
+  
+    return signupStrategy(req, res);
+  })
+  
+  // GET /login
+  router.get('/login', (req, res) => {
+    res.render('login', { message: req.flash('loginMessage') })
+  })
+
+  // POST /login
+router.post('/login', (req, res) => {
+    var loginProperty = passport.authenticate('local-login', {
+      successRedirect : '/',
+      failureRedirect : '/login',
+      failureFlash : true
+    });
+  
+    return loginProperty(req, res);
+  })
+
+  // GET /logout
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  })
+
+  module.exports = router
 
