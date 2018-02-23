0e4eb73c99b09dd8e2e853c2fd1f31bab284006d
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-02-23 15:11:32 -0500

    modified index.js to include passport/sessions etc.

---
+++ controllers/user.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const passport = require('passport')
+
+
