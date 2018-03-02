f222fbe41b5cac8baa722a18048b85cf9ec56da3
Kevin Buckley <kevin.buckley2013@gmail.com>
2018-03-02 11:28:12 -0500

    added port for heroku

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const hbs = require('hbs')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const hbs = require("hbs");
 const methodOverride = require("method-override");
-const session = require('express-session')
-const passport = require('passport')
-const repoController = require('./controllers/repolink')
-const userController = require('./controllers/user')
-const flash = require('connect-flash')
-const cookieParser = require('cookie-parser')
-const morgan = require('morgan')
+const session = require("express-session");
+const passport = require("passport");
+const repoController = require("./controllers/repolink");
+const userController = require("./controllers/user");
+const flash = require("connect-flash");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 
-app.set('view engine', 'hbs')
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(morgan('dev'))
+app.set("view engine", "hbs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
-app.use(cookieParser())
-app.use(bodyParser())
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.use(cookieParser());
+app.use(bodyParser());
 
-require('./config/passport')(passport)
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(flash())
+require("./config/passport")(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
-app.use(function (req, res, next) {
-    res.locals.currentUser = req.user
-    next()
-})
-app.use(session({secret: 'secret'}))
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+app.use(session({ secret: "secret" }));
 
 app.use(methodOverride("_method"));
-app.use('/repolinks', repoController)
-app.use('/user', userController)
+app.use("/repolinks", repoController);
+app.use("/user", userController);
 
 app.set("port", process.env.PORT || 3001);
 
