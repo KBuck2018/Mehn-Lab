c1c261396b05e101d2f9a053647b76078eada593
Kevin Buckley <kevin.buckley2013@gmail.com>
2018-03-02 11:21:59 -0500

    change connection

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,11 @@
 //Kevin's contribution
 var mongoose = require('mongoose');
 
+if(process.env.NODE_ENV === "production"){
+    mongoose.connect(process.env.MLAB_URL)
+} else {
 mongoose.connect("mongodb://localhost/repoLink");
-
+}
 mongoose.Promise = Promise;
 
 module.exports = mongoose;
