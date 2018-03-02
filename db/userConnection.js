ae54cca156c79140d0590e96e6fe5f3a9ff9bae1
Kevin Buckley <kevin.buckley2013@gmail.com>
2018-03-02 11:36:24 -0500

    added connection for heroku

--- db/userConnection.js
+++ db/userConnection.js
@@ -1,7 +1,10 @@
-const userConnection = require('mongoose');
+const userConnection = require("mongoose");
 
-userConnection.connect('mongodb://localhost/passport');
-
+if (process.env.NODE_ENV === "production") {
+  mongoose.connect(process.env.MLAB_URL);
+} else {
+  userConnection.connect("mongodb://localhost/passport");
+}
 userConnection.Promise = Promise;
 
 module.exports = userConnection;
